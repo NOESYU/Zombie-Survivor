31faf03340eed0a9616c6630fd1f1468ebbdd995
dprnd <ssunuu114@gmail.com>
2022-04-12 16:03:12 +0900

    Add Player move and rotate

--- Zombie/Assets/02.Scripts/PlayerMovement.cs
+++ Zombie/Assets/02.Scripts/PlayerMovement.cs
@@ -10,7 +10,7 @@ public class PlayerMovement : MonoBehaviour
     private PlayerInput playerInput; // 플레이어 입력을 알려주는 컴포넌트
 
     [SerializeField] private Rigidbody playerRb;
-    private Animator playerAni;
+    [SerializeField]private Animator playerAni;
 
     private void Start()
     {
@@ -22,16 +22,28 @@ public class PlayerMovement : MonoBehaviour
     // FixedUpdate() : 물리 갱신 주기에 맞춰서 실행됨
     private void FixedUpdate()
     {
-           
+        // 물리정보갱신 주기마다 실행됨 (0.02초)
+
+        Rotate();
+        Move();
+
+        // 입력값에 따라 애니메이터 Move 파라미터 값 변경
+        playerAni.SetFloat("Move", playerInput.move);
     }
 
     private void Move()
     {
-
+        // 상대적으로 이동할 거리 계산
+        Vector3 moveDistance = playerInput.move * transform.forward * moveSpeed * Time.deltaTime;
+        // rb를 이용하여 게임 오브젝트 위치 변경
+        playerRb.MovePosition(playerRb.position + moveDistance);
     }
 
     private void Rotate()
     {
-
+        // 상대적으로 회전할 수치 계산
+        float turn = playerInput.rotate * rotateSpeed * Time.deltaTime;
+        // rb를 이용하여 게임 오브젝트 회전 변경
+        playerRb.rotation = playerRb.rotation * Quaternion.Euler(0, turn, 0f);
     }
 }
